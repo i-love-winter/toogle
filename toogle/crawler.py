5afa27c9791bf9adb140258cb5c614f0aa71e80e
aether123 <imatoaster65@gmail.com>
2025-09-02 15:09:52 +1000

    havent tested yet, will fix later
    :p

---
+++ toogle/crawler.py
@@ -0,0 +1,22 @@
+# import necessary libraries
+import requests
+from bs4 import BeautifulSoup
+
+# define the seed url
+seedurl = "www.google.com"
+
+# send a HTTP request to the url
+response = requests.get(seedurl)
+
+# parse the HTML content of the page
+hmtl_content = response.text
+soup = BeautifulSoup(hmtl_content, 'html.parser')
+
+# discovering and saving links inside the HTML content
+
+links = [] # creating a list where the urls to scan will go
+for a_tag in soup.find_all('a'):
+    href = a_tag.get('href')
+    if href: # checking that href actually exists
+        links.append(href)
+
