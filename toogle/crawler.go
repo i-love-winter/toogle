b4da555acdae061ced1385aca1097c671326a0f5
aether123 <imatoaster65@gmail.com>
2025-09-09 09:55:24 +1000

    YAYAYAYAYAYYAYAYAYA

---
+++ toogle/crawler.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+// create a map to store visited urls to ensure that you don't visit the same url twice
+var visitedurls = make(map[string]bool)
+
+func main() {
+	// define the seed url
+	seedurl := "https://en.wikipedia.org/wiki/Spider"
+
+	// call the crawl function
+	crawl(seedurl, 0)
+}
+
+func crawl(currenturl string, maxdepth int) {
+	// create a new collector
+	c := colly.NewCollector(
+		colly.MaxDepth(maxdepth),
+	)
+
+	// extract and log the page title
+	c.OnHTML("title", func(e *colly.HTMLElement) {
+		fmt.Println("Page Title:", e.Text)
+	})
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// get absolute URL
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		// check if current URL has already been visited
+		if link != "" && !visitedurls[link] {
+			// add current URL to visitedURLs
+			visitedurls[link] = true
+			fmt.Println("Found link:", link)
+			// visit current URL
+			e.Request.Visit(link)
+		}
+	})
+
+	// add an OnRequest callback to track progress
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Crawling", r.URL)
+	})
+
+	// handle errors
+	c.OnError(func(e *colly.Response, err error) {
+		fmt.Println("Request URL:", e.Request.URL, "failed with response:", e, "\nError:", err)
+	})
+
+	// visit the seed url
+	err := c.Visit(currenturl)
+	if err != nil {
+		fmt.Println("Error visiting page:", err)
+	}
+}
